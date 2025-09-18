d4e6188095da57849a10f65724779734ed028f60
KOOTIK <kootgm@gmail.com>
2025-09-18 19:34:49 +0300

    FZ152

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,4 +19,4 @@ def test_index_route(client):
     """
     response = client.get('/')
     assert response.status_code == 200
-	assert "Основы информационной безопасности".encode('utf-8') in response.data
+    assert "Основы информационной безопасности".encode('utf-8') in response.data
